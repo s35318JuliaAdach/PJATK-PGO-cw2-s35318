841bcf7ad5dcdec49d11318212a352b2907ab420
s35318JuliaAdach <35318@pjwstk.edu.pl>
2026-04-01 14:04:00 +0200

    Ddodanie klasy książka

--- src/Ksiazka.java
+++ src/Ksiazka.java
@@ -1,19 +1,15 @@
 public class Ksiazka {
-    // Pola (cechy książki)
     private String tytul;
     private String autor;
     private int liczbaStron;
     private boolean dostepna;
 
-    // Konstruktor (maszyna do tworzenia książki)
     public Ksiazka(String tytul, String autor, int liczbaStron, boolean dostepna) {
         this.tytul = tytul;
         this.autor = autor;
         this.liczbaStron = liczbaStron;
         this.dostepna = dostepna;
     }
-
-    // Metody (co książka potrafi)
     public void wypiszInfo() {
         System.out.println("Tytuł: " + tytul + " | Autor: " + autor + " | Stron: " + liczbaStron + " | Dostępna: " + (dostepna ? "Tak" : "Nie"));
     }
